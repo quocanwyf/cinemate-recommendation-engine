85698f5e52d5a7cab06b45877dc0ba7f2f81c24c
Quoc An <quocan@gmail.com>
2025-10-11 14:30:31 +0700

    fix: id file sh

--- download_models.sh
+++ download_models.sh
@@ -10,14 +10,14 @@ echo ">>> Downloading SVD model..."
 # Lấy link chia sẻ của Google Drive và trích xuất ID file
 # Ví dụ link: https://drive.google.com/file/d/FILE_ID/view?usp=sharing
 # Thay FILE_ID_SVD bằng ID thật của bạn
-gdown --id https://drive.google.com/file/d/1cw4trFOJcKElY_VVBuvmQAvKT38vx9UA/view?usp=drive_link -O models/svd_production_model
+gdown --id 1cw4trFOJcKElY_VVBuvmQAvKT38vx9UA -O models/svd_production_model
 
 echo ">>> Downloading Cosine Similarity matrix..."
 # Thay FILE_ID_COSINE bằng ID thật của bạn
-gdown --id https://drive.google.com/file/d/1pegfmlxEaAWft1EKUP8eqXzbRKbJ0I9G/view?usp=drive_link -O models/cosine_similarity_matrix.npy
+gdown --id 1pegfmlxEaAWft1EKUP8eqXzbRKbJ0I9G -O models/cosine_similarity_matrix.npy
 
 echo ">>> Downloading Movies DataFrame..."
 # Thay FILE_ID_DATAFRAME bằng ID thật của bạn
-gdown --id https://drive.google.com/file/d/1wwrl_48EJ39HCFtjjMf7Qy3pHl5nKhH5/view?usp=drive_link -O models/movies_df_for_similarity.pkl
+gdown --id 1wwrl_48EJ39HCFtjjMf7Qy3pHl5nKhH5 -O models/movies_df_for_similarity.pkl
 
 echo ">>> Model download complete."
